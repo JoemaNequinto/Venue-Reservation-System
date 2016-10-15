42013fdd06f4dc41ccc3bfe2b2cd8fd3da93a69b
JoemaNequinto <janequinto@up.edu.ph>
2016-10-15 17:22:38 +0800

    Edited schema.sql

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const __ = require(__dirname + '/../controllers/');
+const __ = require(__dirname + '/../controllers/user');
 
 module.exports = (router) => {
 
 	router.del = router.delete;
 
-	router.post('/api/login', __.user.login);
-	router.get('/api/logout', __.user.logout);
+	router.post('/api/login', __.login);
+	router.get('/api/logout', __.logout);
 
 	router.all('*', (req, res) => {
 		res.status(404).send({message : 'Unmatched route. =(('});
