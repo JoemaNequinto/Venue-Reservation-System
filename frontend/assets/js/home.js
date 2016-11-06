a46c037beb3f8282f82edf35e45cbdf86e38161e
gelloguiam <acguiam@gmail.com>
2016-11-06 12:11:01 +0800

    modified login page, removed template sidenave from login

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -2,19 +2,7 @@ $(document).ready(function(){
   setSideNav();
 });
 
-function renderVenueMap() {
-  var map_container = $("#venue-map")[0];
-  var map_attributes = {
-     center: new google.maps.LatLng(14.1655209, 121.2396087),
-     zoom: 15
-//     mapTypeId: 'satellite'
-   }
-
-  var map = new google.maps.Map(map_container, map_attributes);
-  console.log(map);
-}
-
 function setSideNav(){
-  $("#side-nav-home").removeClass("active-nav");
-  $("#side-nav-search").addClass("active-nav");
+  $("#side-nav-search").removeClass("active-nav");
+  $("#side-nav-home").addClass("active-nav");
 }
--- frontend/assets/js/profile.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  setSideNav();
-});
-
-function setSideNav(){
-  $("#side-nav-search").removeClass("active-nav");
-  $("#side-nav-home").addClass("active-nav");
-}
