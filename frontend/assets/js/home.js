f12551e05966d65b551c44fd7acb4b8a0d00dc1b
gelloguiam <acguiam@gmail.com>
2016-11-13 19:08:49 +0800

    infor for adding venue and event

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
 
-    console.log("reading script");
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+      });
+
+    $('select').material_select();
     // $('#add-venue').modal();
     setSideNav();
 
     $("#add-venue-trig").click(function(){
-        console.log("clicked!");
+        $(".modal-card").fadeOut("fast");
+        $("#add-venue").fadeIn("fast");
     });
 
-    $("#overlay").hover(function(){
+    $("#add-event-trig").click(function(){
+        $(".modal-card").fadeOut("fast");
+        $("#add-event").fadeIn("fast");
+    });
+
+    $("#overlay").click(function(){
         $(this).hide();
+        $(".modal-card").fadeOut("fast");
     });
 
     $("#add-fab").hover(function(){
         $("#overlay").fadeTo(100, 0.8);
+    });
+
+    $("#overlay").hover(function(){
+        if(!$(".modal-card").is(":visible")) {
+            $(this).hide();
+        }
     });
 
 });
