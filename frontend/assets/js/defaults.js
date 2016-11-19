9b41cb65d90c6d548feaf9719e401afd06fc3bd1
gelloguiam <acguiam@gmail.com>
2016-11-19 15:12:31 +0800

    fixed nav script

--- frontend/assets/js/defaults.js
+++ frontend/assets/js/defaults.js
@@ -1,30 +1,11 @@
-$('.button-collapse').sideNav({
-	menuWidth: 300, // Default is 240
-	edge: 'left', // Choose the horizontal origin
-	closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
-});
-
-$(document).ready(function(){
-	$("#nav-toggle").click(function(){
-		toggleNavigation();
-	});
-
-	$("#side-nav-home").click(function(){
-		window.location.href = "\#\home";
-	});
-
-	$("#side-nav-search").click(function(){
-		window.location.href = "\#\search";
-	});
-
-	$("#side-nav-logout").click(function(){
-		logout();
-	});
-
-
-});
+// $('.button-collapse').sideNav({
+// 	menuWidth: 300, // Default is 240
+// 	edge: 'left', // Choose the horizontal origin
+// 	closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
+// });
 
 function toggleNavigation() {
+
 	var total_width = $("#main-wrapper").width();
 	var reference_width = $("#sidebar-wrapper").width();
 
@@ -32,6 +13,7 @@ function toggleNavigation() {
 		$("#sidebar-wrapper").width("5%");
 		$("#content-wrapper").width("95%");
 	}
+
 	else {
 		$("#sidebar-wrapper").width("15%");
 		$("#content-wrapper").width("85%");
@@ -41,3 +23,11 @@ function toggleNavigation() {
 	$("#site-title").toggle();
 	$(".nav-item-text").toggle();
 }
+
+$(document).ready(function(){
+
+	$("#nav-toggle").click(function(){
+		toggleNavigation();
+	});
+
+});
--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
 
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year
-        format: 'yyyy-mm-dd'
-      });
-
-    $('select').material_select();
-    setSideNav();
-
     $("#add-fab").click(function(){
         $("#overlay").fadeTo(100, 0.8);
         $(".modal-card").fadeOut("fast");
@@ -31,7 +22,18 @@ $(document).ready(function(){
         $(".modal-card").fadeOut("fast");
     });
 
+    // $('.datepicker').pickadate({
+    //     selectMonths: true, // Creates a dropdown to control month
+    //     selectYears: 15, // Creates a dropdown of 15 years to control year
+    //     format: 'yyyy-mm-dd'
+    //   });
+    //
+    // $('select').material_select();
+    // setSideNav();
+
+
 });
+
 function setSideNav(){
     $('#side-nav-search').removeClass('active-nav');
     $('#side-nav-home').addClass('active-nav');
